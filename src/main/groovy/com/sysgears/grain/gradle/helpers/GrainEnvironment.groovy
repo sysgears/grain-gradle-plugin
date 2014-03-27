9128bcf62d706ebba3087751e00494b9ef0f0ff1
Dmitriy Pavlenko <dmitriy.pavlenko@sysgears.com>
2014-03-27 10:32:12 +0200

    Resolve the application.properties file path relative to the project directory, so that plugin works properly with IDEA JetGradle. Signed-off-by: Dmitriy Pavlenko <dmitriy.pavlenko@sysgears.com>

--- src/main/groovy/com/sysgears/grain/gradle/helpers/GrainEnvironment.groovy
+++ src/main/groovy/com/sysgears/grain/gradle/helpers/GrainEnvironment.groovy
@@ -19,15 +19,16 @@ class GrainEnvironment {
      * @return the value of the project property if found, the default value otherwise
      */
     static String lookUpProperty(Project project, String name, String value) {
-        def specifiedGrainVersion = null
-        def grainPropertiesFile = project.grain.projectDir ?
-            new File(project.grain.projectDir as String, PROPERTIES_FILE) : new File(PROPERTIES_FILE)
-        if (grainPropertiesFile.exists() && grainPropertiesFile.isFile()) {
-            def grainProps = new Properties()
-            grainProps.load(new FileInputStream(grainPropertiesFile))
-            specifiedGrainVersion = grainProps.getProperty(name)
+        def property = null
+        def propertiesFile = project.file(project.grain.projectDir ?
+            "${project.grain.projectDir}/${PROPERTIES_FILE}" : PROPERTIES_FILE)
+
+        if (propertiesFile.exists() && propertiesFile.isFile()) {
+            def props = new Properties()
+            props.load(new FileInputStream(propertiesFile))
+            property = props.getProperty(name)
         }
 
-        specifiedGrainVersion ?: value
+        property ?: value
     }
 }
